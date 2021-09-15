4b9dd161549639d5de6b5e2dfcf6e290e4d18daa
Demchenko Kirill <thegalaxydias@gmail.com>
2021-09-15 17:22:37 +0300

    Добавьте файлы проекта.

---
+++ Overload/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Overload
+{
+    class Program
+    {
+
+        static int CreateMessage(int a, int b)
+        {
+
+            return a + b;
+        }
+
+        static string CreateMessage()
+        {
+            string message = "Hello";
+            return message;
+        }
+
+        static void Main(string[] args)
+        {
+            string result = CreateMessage();
+            Console.WriteLine(result);
+        }
+
+       
+    }
+
+   
+  
+
+}
